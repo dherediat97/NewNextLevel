87a04352230887adef94b5ec901628d27dcc1a3c
dherediat <dherediat97@gmail.com>
2021-12-19 12:07:30 +0100

    Obtener campeones del lol

--- lib/api.dart
+++ lib/api.dart
@@ -1,34 +1,34 @@
+import 'dart:convert';
 import 'package:newnextlevel/model/lol_champion.dart';
 import 'package:newnextlevel/model/lol_response.dart';
 import 'dart:async';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Api {
-  // List<LolChampion> champions = [];
-  // Future<List<LolChampion>> getAllLolChampions(
-  //     var version, var language) async {
-  //   final response = await http.get(Uri.dataFromString(
-  //       "http://ddragon.leagueoflegends.com/cdn/" +
-  //           version +
-  //           "/data/" +
-  //           language +
-  //           "/champion.json"));
-  //   if (response.statusCode == 200) {
-  //     var responseBody = LolResponse.fromJson(json.decode(response.body));
-  // var champions = getChampions(responseBody);
-  //     return champions;
-  //   } else {
-  //     throw Exception('Failed to load post');
-  //   }
-  // }
+  List<LolChampion> champions = [];
+  String baseUrlDDragon = "http://ddragon.leagueoflegends.com/cdn/";
 
-  // List<LolChampion> getChampions(LolResponse response) {
-  //   var keysChampions = response.data.keys.toList();
-  //   for (var value in keysChampions) {
-  //     LolChampion champion = LolChampion.fromJson(response.data);
-  //     champions.add(champion);
-  //   }
-  //   return champions;
-  // }
+  Future<List<LolChampion>> getAllLolChampions(
+      var version, var language) async {
+    final response = await http
+        .get(baseUrlDDragon + version + "/data/" + language + "/champion.json");
+    if (response.statusCode == 200) {
+      var data = LolResponse.fromJson(jsonDecode(response.body));
+      var champions = getChampions(data);
+      return champions;
+    } else {
+      throw Exception('Failed to load post');
+    }
+  }
+
+  List<LolChampion> getChampions(LolResponse response) {
+    const JsonEncoder encoder = JsonEncoder.withIndent('  ');
+    var keysChampions = response.data.keys.toList();
+    for (var value in keysChampions) {
+      String str = encoder.convert(response.data[value]);
+      var champion = LolChampion.fromJson(jsonDecode(str));
+      champions.add(champion);
+    }
+    return champions;
+  }
 }
